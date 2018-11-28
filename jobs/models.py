27708c6901c82071814b50aeac18bb0f28e09fa0
Navin <nntheprince866@gmail.com>
2018-11-28 00:22:59 -0800

    app up and running

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Job(models.Model):
+    """docstring for Job.models.Model
+    def __init__(self, arg):
+        super(Job,models.Model).__init__()
+        self.arg = arg
+    """
+
+    image = models.ImageField(upload_to='images/')
+    summary = models.CharField(max_length=200)
